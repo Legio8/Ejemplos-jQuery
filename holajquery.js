6ab932a64d84625e24891cc180b187f5f0936762
Karen Bautista <karenmtz273@gmail.com>
2017-03-08 12:03:00 -0600

    Se añaden metodos de jQuery

---
+++ holajquery.js
@@ -0,0 +1,10 @@
+//$(document).ready(saluda)
+
+$(function (){
+    //alert($('h1').text())
+
+    //Se crea una nuevo parrafo en jQuery
+    $nuevo = $('<p> Parrafo <i><b>nuevo</b></i> </p>')
+    $('h2').after($nuevo)
+
+})
